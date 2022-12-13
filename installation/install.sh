1742e07eb09f47a53643429fd75fa46153c10bf7
Jonas Örnfelt <archornf@gmail.com>
2022-12-13 19:06:01 +0100

    Script updates and hyprland key changes

--- installation/install.sh
+++ installation/install.sh
@@ -3,5 +3,5 @@
 # replace picom-pijulius-git with regular picom if you wish...
 
 #for x in $(cat pk5); do yay -S --noconfirm $x; done
-for x in $(cat packages.txt); do yay -S --noconfirm $x; done
+for x in $(cat pk1); do yay -S --noconfirm $x; done
 
