652b1b6654ca5e742fd99e589e3e9c71bc6f3f24
Jonas Örnfelt <archornf@gmail.com>
2022-12-29 22:14:30 +0100

    Updated Hyprland clipboard manager

--- bin/my_scripts/clip_history.sh
+++ bin/my_scripts/clip_history.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sleep 0.1
-/usr/bin/diodon
+
+# If you run this command with an argument, it will use greenclip
+if [ -n "$1" ]; then
+	rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
+else
+	#sleep 0.1
+	/usr/bin/diodon
+fi
