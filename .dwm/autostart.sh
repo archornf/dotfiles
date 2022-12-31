d50cb92e80b09786edd9edccf20fd0c45e4c81c2
Jonas Örnfelt <archornf@gmail.com>
2022-12-31 11:10:17 +0100

    dwm clip_his update, picom and cava...

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -7,7 +7,7 @@ feh --bg-fill --randomize ~/Pictures/Wallpapers/* &
 
 #arr=("xfce4-power-man" "copyq" "fcitx5" "dunst" "clipmenud" "qv2ray" "redshift-gtk" "mpd" "picom" "qbittorrent" "nutstore" "solaar")
 # arr=("xfce4-power-manager" "diodon" "dwmblocks", "firefox")
-arr=("diodon")
+# arr=("diodon")
 
 for value in ${arr[@]}; do
   if [[ ! $(pgrep ${value}) ]]; then
@@ -17,4 +17,7 @@ done
 
 # Wake dwmblocks
 sleep 0.5
+kill -44 $(pidof dwmblocks)
+greenclip daemon
+sleep 1
 kill -44 $(pidof dwmblocks)
