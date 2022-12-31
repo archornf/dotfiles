d87b716c77c421f38c032d88465f76dc4f42805b
Jonas Örnfelt <archornf@gmail.com>
2022-12-31 12:38:16 +0100

    dmenu centered

---
+++ .config/dmenu/config.h
@@ -0,0 +1,42 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+
+// https://tools.suckless.org/dmenu/patches/center/dmenu-center-5.2.diff
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10",
+	"NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
+};
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+// https://tools.suckless.org/dmenu/patches/border/dmenu-border-4.9.diff
+/* Size of the window border */
+static const unsigned int border_width = 5;
--- bin/my_scripts/clip_history.sh
+++ bin/my_scripts/clip_history.sh
@@ -4,6 +4,9 @@
 # If you run this command with an argument, it will use greenclip
 if [ -n "$1" ]; then
 	rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
+	# Dmenu
+	# greenclip print | grep . | dmenu -i -l 10 -p clipboard | xargs -r -d'\n' -I '{}' greenclip print '{}'
+	# greenclip print | grep . | fzf -e | xargs -r -d'\n' -I '{}' greenclip print '{}'
 else
 	#sleep 0.1
 	/usr/bin/diodon
