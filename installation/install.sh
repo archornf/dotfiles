7615f27f3167181d280a2e729f68d4f53a641e97
Jonas Örnfelt <archornf@gmail.com>
2022-12-31 12:46:57 +0100

    updated install docs

--- installation/install.sh
+++ installation/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-# You can skip rtl8852be-dkms-git and 
+# You can skip rtl8852be-dkms-git / rtw89... and 
 # replace picom-pijulius-git with regular picom if you wish...
 
 #for x in $(cat pk5); do yay -S --noconfirm $x; done
-for x in $(cat pk1); do yay -S --noconfirm $x; done
+for x in $(cat packages.txt); do yay -S --noconfirm $x; done
 
