725a7436660e6a1b7c1e5ca8a74f009e3eac3da2
Jonas Örnfelt <archornf@gmail.com>
2023-01-16 10:14:49 +0100

    progrm_helper

---
+++ bin/my_scripts/progrm_helper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# rofi_command="rofi -theme $dir/powermenu.rasi"
+rofi_command="rofi -theme ~/.config/rofi/themes/gruvbox/gruvbox-dark.rasi"
+
+# Options
+compOpt="complexities"
+dpOpt="design patterns"
+dsOpt="data structures"
+
+# Variable passed to rofi
+options="$compOpt\n$dpOpt\n$dsOpt"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Choose a command" -dmenu -selected-row 0)"
+case $chosen in
+    $compOpt)
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/complexities.txt; zsh'
+        ;;
+    $dpOpt)
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/dp.txt; zsh'
+        ;;
+    $dsOpt)
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/ds.txt; zsh'
+        ;;
+esac
