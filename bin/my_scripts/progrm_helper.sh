a3b96855ce3957fce207e4ad78960aa711fd2218
Jonas Örnfelt <archornf@gmail.com>
2023-02-10 17:52:08 +0100

    progrm_helper updates

--- bin/my_scripts/progrm_helper.sh
+++ bin/my_scripts/progrm_helper.sh
@@ -5,14 +5,16 @@ rofi_command="rofi -theme ~/.config/rofi/themes/gruvbox/gruvbox-dark.rasi"
 
 # Options
 compOpt="complexities"
+csdOpt="csd"
 dpOpt="design patterns"
 dsOpt="data structures"
+dtOpt="data types"
 genOpt="general"
 praOpt="practical"
 priOpt="principles"
 
 # Variable passed to rofi
-options="$compOpt\n$dpOpt\n$dsOpt\n$genOpt\n$praOpt\n$priOpt"
+options="$compOpt\n$csdOpt\n$dpOpt\n$dsOpt\n$dtOpt\n$genOpt\n$praOpt\n$priOpt"
 
 # Dmenu
 #chosen="$(echo -e "$options" | dmenu -i -p "Choose a command" -l 10)"
@@ -23,21 +25,28 @@ chosen="$(echo -e "$options" | $rofi_command -p "Choose a command" -dmenu -selec
 case $chosen in
     $compOpt)
 		# ; zsh keeps the terminal alive after closing document
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/complexities.txt; zsh'
+		#$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/complexities.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/complexities.txt'
+        ;;
+    $csdOpt)
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/csd.txt'
         ;;
     $dpOpt)
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/dp.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/dp.txt'
         ;;
     $dsOpt)
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/ds.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/ds.txt'
+        ;;
+    $dtOpt)
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/dt.txt'
         ;;
     $genOpt)
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/general.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/general.txt'
         ;;
     $praOpt)
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/practical.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/practical.txt'
         ;;
     $priOpt)
-		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/principles.txt; zsh'
+		$1 -e bash -c 'nvim ~/Documents/progrm_help_docs/principles.txt'
         ;;
 esac
