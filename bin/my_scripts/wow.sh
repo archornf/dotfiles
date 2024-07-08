7902782b0a5be7b20ecbdb06b02ae4840d809ce1
ornfelt <jonasornfelt@outlook.com>
2024-07-08 15:31:29 +0200

    script updates

--- bin/my_scripts/wow.sh
+++ bin/my_scripts/wow.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+# output=$(ps aux | grep -E "/home/jonas/(acore/bin/worldserver|tcore/bin/worldserver|cmangos/run/bin/mangosd|vmangos/bin/mangosd)" | grep -v grep)
+# 
+# # Check if output contains any of the servers
+# if echo "$output" | grep -q "/home/jonas/acore/bin/worldserver"; then
+#   echo "acore server is running"
+# elif echo "$output" | grep -q "/home/jonas/tcore/bin/worldserver"; then
+#   echo "tcore server is running"
+# elif echo "$output" | grep -q "/home/jonas/cmangos/run/bin/mangosd"; then
+#   echo "cmangos server is running"
+# elif echo "$output" | grep -q "/home/jonas/vmangos/bin/mangosd"; then
+#   echo "vmangos server is running"
+# else
+#   echo "No server is running"
+# fi
+
 # Paths to Wow.exe
 WOW_PATH_MOUNTED="/mnt/new/wow/Wow.exe"
 WOW_PATH_DOWNLOADS="$HOME/Downloads/wow/Wow.exe"
