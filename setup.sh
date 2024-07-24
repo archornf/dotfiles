4438cefa91f5651472679805fd252fc490e97767
ornfelt <jonasornfelt@outlook.com>
2024-07-24 10:48:46 +0200

    setup script fixes

--- setup.sh
+++ setup.sh
@@ -908,41 +908,6 @@ compile_projects() {
 
     if check_file "OpenDiablo2" "OpenDiablo2"; then
         source build.sh
-        # Fix config.json if it exists...
-        if [ -f "$HOME/.config/OpenDiablo2/config.json" ]; then
-            DIABLO_DIRS=(
-                "/mnt/new/d2/"
-                "$HOME/Downloads/d2/"
-                #"/media/2024/d2/"
-            )
-
-            NEW_PATH=""
-            # Check each directory and set NEW_PATH to the first one that exists
-            for DIR in "${DIABLO_DIRS[@]}"; do
-                if [ -d "$DIR" ]; then
-                    NEW_PATH="$DIR"
-                    break
-                fi
-            done
-
-            # Check if a new path was found
-            if [ -z "$NEW_PATH" ]; then
-                echo "No valid Diablo 2 directory found."
-            fi
-
-            if [ -n "$NEW_PATH" ]; then
-                NEW_PATH="${NEW_PATH}d2video"
-            fi
-
-            ESCAPED_NEW_PATH=$(echo "$NEW_PATH" | sed 's/\\/\\\\/g')
-
-            # Update config.json file with new path
-            sed -i "s|\"MpqPath\": \".*\"|\"MpqPath\": \"$ESCAPED_NEW_PATH\"|g" $HOME/.config/OpenDiablo2/config.json
-            echo "Updated config.json with new MpqPath path: $NEW_PATH"
-        else
-            echo "$HOME/.config/config.json does not exist yet. OpenDiablo2 has not been run yet..."
-        fi
-
         cd "$HOME/Code2/Go"
     fi
 
@@ -1383,6 +1348,41 @@ copy_game_data() {
 
     # Diablo 2
     copy_dir_to_target "$MEDIA_PATH/2024/d2" "$HOME/Downloads/d2"
+
+    # Fix OpenDiablo2 config.json if it exists...
+    if [ -f "$HOME/.config/OpenDiablo2/config.json" ]; then
+        DIABLO_DIRS=(
+            "/mnt/new/d2/"
+            "$HOME/Downloads/d2/"
+            #"/media/2024/d2/"
+        )
+
+        NEW_PATH=""
+        # Check each directory and set NEW_PATH to the first one that exists
+        for DIR in "${DIABLO_DIRS[@]}"; do
+            if [ -d "$DIR" ]; then
+                NEW_PATH="$DIR"
+                break
+            fi
+        done
+
+        # Check if a new path was found
+        if [ -z "$NEW_PATH" ]; then
+            echo "No valid Diablo 2 directory found."
+        fi
+
+        if [ -n "$NEW_PATH" ]; then
+            NEW_PATH="${NEW_PATH}d2video"
+        fi
+
+        ESCAPED_NEW_PATH=$(echo "$NEW_PATH" | sed 's/\\/\\\\/g')
+
+        # Update config.json file with new path
+        sed -i "s|\"MpqPath\": \".*\"|\"MpqPath\": \"$ESCAPED_NEW_PATH\"|g" $HOME/.config/OpenDiablo2/config.json
+        echo "Updated config.json with new MpqPath path: $NEW_PATH"
+    else
+        echo "$HOME/.config/config.json does not exist yet. OpenDiablo2 has not been run yet..."
+    fi
 
     # TODO:
     # Copy ollama models?
