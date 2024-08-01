ea4a68a9f220767c94ccf180bb8ed1d0f7c251da
ornfelt <jonasornfelt@outlook.com>
2024-08-01 07:20:52 +0200

    eclipse notes

--- bin/my_scripts/screenshot.sh
+++ bin/my_scripts/screenshot.sh
@@ -2,4 +2,5 @@
 export DISPLAY=:0
 # import -window root ~/Pictures/Screenshots/Screenshot-$(date --iso-8601=seconds).png
 #scrot ~/Pictures/Screenshots/Screenshot-$(date --iso-8601=seconds).png
-maim /home/jonas/Pictures/Screenshots/Screenshot-$(date --iso-8601=seconds).png 2> /home/jonas/screen_err
+#maim /home/jonas/Pictures/Screenshots/Screenshot-$(date --iso-8601=seconds).png 2> /home/jonas/screen_err
+maim /mnt/new/Pictures/Screenshots/Screenshot-$(date --iso-8601=seconds).png 2> /home/jonas/screen_err
